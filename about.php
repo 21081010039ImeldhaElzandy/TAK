fe2534d59bbfac8153a2cc8f7268f3f7335df77c
21081010039ImeldhaElzandy <21081010039@student.upnjatin.ac.id>
2023-10-09 15:36:28 +0700

    first commit

---
+++ about.php
@@ -0,0 +1,49 @@
+<!-- Masthead-->
+<header class="masthead">
+    <div class="container h-100">
+        <div class="row h-100 align-items-center justify-content-center text-center">
+            <div class="col-lg-10 align-self-end mb-4" style="background: #0000002e;">
+                <h2 class="text-white">Tentang Kami</h2>
+                <hr class="divider my-4" />
+            </div>
+
+        </div>
+    </div>
+</header>
+
+<section class="page-section">
+    <div class="container">
+        <!-- <?php echo html_entity_decode($_SESSION['setting_about_content']) ?> -->
+
+        <h2 style="font-size:28px;background: transparent; position: relative;">TAK - Toko Alat Kesehatan</h2>
+        <p style="text-align: left; margin-bottom: 15px; padding: 0px;">
+            <font color="#000000" face="Open Sans, Arial, sans-serif">Toko Alat Kesehatan kami telah melayani 
+                kebutuhan kesehatan masyarakat selama lebih dari satu dekade. Kami mengutamakan kesejahteraan 
+                dan kenyamanan pelanggan kami dengan menyediakan berbagai alat kesehatan berkualitas tinggi 
+                dan pelayanan terbaik. Tim kami yang berdedikasi dan berpengalaman akan membantu Anda menemukan 
+                alat-alat kesehatan yang sesuai dengan kebutuhan Anda. Kami berkomitmen untuk memberikan produk 
+                berkualitas dan pelayanan yang memuaskan, sehingga Anda dapat merasa aman dan nyaman dalam menjaga 
+                kesehatan Anda dan orang yang Anda cintai.</font>
+        </p>
+        <p style="text-align: left; margin-bottom: 15px; padding: 0px;">
+            <font color="#000000" face="Open Sans, Arial, sans-serif">Toko Alat Kesehatan kami memiliki pandangan 
+                yang sama tentang pentingnya kesehatan dan kesejahteraan Anda. Kami percaya bahwa alat kesehatan adalah 
+                aspek penting dalam menjaga gaya hidup sehat. Dengan lebih dari satu dekade pengalaman, Toko kami telah 
+                menjadi tujuan terpercaya untuk semua kebutuhan kesehatan Anda. Kami berusaha menciptakan pengalaman yang 
+                nyaman dan aman bagi pelanggan kami. Di Toko kami, Anda akan menemukan berbagai pilihan peralatan kesehatan 
+                berkualitas tinggi yang dirancang untuk memenuhi berbagai kebutuhan Anda. </font>
+        </p>
+        <p style="text-align: left; margin-bottom: 15px; padding: 0px;">
+            <font color="#000000" face="Open Sans, Arial, sans-serif">Kami juga memiliki tim yang berpengalaman dan siap 
+                membantu Anda dalam memilih produk yang sesuai dengan kebutuhan Anda. Terima kasih atas kepercayaan Anda 
+                selama ini. Kami berkomitmen untuk terus mendukung kesehatan dan kesejahteraan Anda dengan menyediakan 
+                produk dan layanan terbaik. Bergabunglah dengan kami dan jadilah bagian dari komunitas yang peduli tentang 
+                kesehatan, karena kesehatan Anda adalah prioritas utama kami.
+                Terima kasih atas kepercayaan Anda kepada TAK kami. Kami berharap Anda dapat menikmati produk dan layanan kami 
+                dengan maksimal. Semoga Anda selalu sehat dan bahagia! Terima kasih dan selamat menggunakan layanan TAK kami.</font>
+        </p>
+        <p style="text-align: left; margin-bottom: 15px; padding: 0px;">
+            <font color="#000000" face="Open Sans, Arial, sans-serif">Tim TAK - Toko Alat Kesehatan</font>
+        </p>
+    </div>
+</section>
